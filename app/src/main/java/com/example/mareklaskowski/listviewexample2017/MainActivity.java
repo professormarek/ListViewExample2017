8be98d2aed86a9b0d84cd2e4ad020d6d9a2d4a40
marek.laskowski <marek.laskowski@senecacollege.ca>
2017-01-31 12:42:59 -0500

    added listeners for list clicks

--- app/src/main/java/com/example/mareklaskowski/listviewexample2017/MainActivity.java
+++ app/src/main/java/com/example/mareklaskowski/listviewexample2017/MainActivity.java
@@ -2,10 +2,32 @@ package com.example.mareklaskowski.listviewexample2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /*
+    create an OnItemClickListener object and override its onItemClick method
+    (this is much like the anonymous class pattern shown earlier with buttons)
+     */
+    private AdapterView.OnItemClickListener myMessageClickHandler = new AdapterView.OnItemClickListener() {
+        @Override
+        /*
+        parent is the ListView
+        view is a way to get the context
+        position is the item that was clicked
+        the android:id of the item that was clicked (?) - look me up in the api
+         */
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            //get data from the view that was clicked
+            String info = parent.getItemAtPosition(position).toString();
+            //all we'll do here is show a toast, but you can imagine other things as well
+            Toast.makeText(view.getContext(), info, Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
         //set the adapter for listView to use our ArrayAdpter "sAdpater"
         listView.setAdapter(sAdapter);
+
+        //assoiciate the listener we create above with our listView
+        listView.setOnItemClickListener(myMessageClickHandler);
 
     }
 }
